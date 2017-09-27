01c8103a5ad07bb5f452e483bbe7433ac616d7ae
martin_modrak <cerny.m@gmail.com>
2017-09-27 16:12:37 +0200

    Centering camera on player

--- src/GameConstants.elm
+++ src/GameConstants.elm
@@ -1,13 +1,12 @@
 module GameConstants exposing(..)
 
-import Matrix3 
 import Vector2 exposing (Float2)
 
 physicsUpdateTime: Float
 physicsUpdateTime = 0.04
 
 physicsTimeWarp : Float
-physicsTimeWarp = 0.3
+physicsTimeWarp = 1
 
 maxTimeAccumulator: Float
 maxTimeAccumulator = 1
@@ -34,7 +33,12 @@ gravity = (0, -10)
 windDirection: Float2
 windDirection = (1, 0)
 
-viewMatrix: Matrix3.Float3x3
-viewMatrix = ((100,  0   , 400),
-              (0   , -100, 600),
-              (0   ,  0   , 1  ))
+viewScaleX : Float
+viewScaleX = 100
+
+viewScaleY : Float
+viewScaleY = -100
+
+playerPosOnScreen : Float2
+playerPosOnScreen = 
+    (400, 600)
--- src/View.elm
+++ src/View.elm
@@ -9,11 +9,11 @@ import Html.Attributes as Attr
 import GameConstants
 
 
-float2ToStyle : Float2 -> List ( String, String )
-float2ToStyle pos =
+float2ToStyle : Matrix3.Float3x3 -> Float2 -> List ( String, String )
+float2ToStyle viewMatrix pos =
     let
         ( x, y ) =
-            Matrix3.transform GameConstants.viewMatrix pos
+            Matrix3.transform viewMatrix pos
     in
         [ ( "left", toString x )
         , ( "top", toString y )
@@ -22,35 +22,60 @@ float2ToStyle pos =
 
 view : Model -> Html Msg.Msg
 view model =
+    let
+        viewMatrix = computeViewMatrix model
+    in
+        
     div [ Attr.class "game" ]
         ((div
             [ Attr.class "kite"
-            , Attr.style (float2ToStyle model.kitePos)
+            , Attr.style (float2ToStyle viewMatrix model.kitePos)
             ]
             []
          )
-            :: (div [ Attr.class "player", Attr.style (float2ToStyle model.playerPos) ] [])
-            :: (div [ Attr.class "windIndicator", Attr.style (float2ToStyle (model.windIndicatorX, 3))] [])
-            :: (div [ Attr.class "windIndicator", Attr.style (float2ToStyle (60, 3))] []) --spacer
-            :: (List.concatMap viewDebugArrow model.debugArrows)
+            :: (div [ Attr.class "player", Attr.style (float2ToStyle viewMatrix model.playerPos) ] [])
+            :: (div [ Attr.class "windIndicator", Attr.style (float2ToStyle viewMatrix ( model.windIndicatorX, 3 )) ] [])
+            :: (div [ Attr.class "windIndicator", Attr.style (float2ToStyle viewMatrix ( 60, 3 )) ] [])
+            :: (div [ Attr.class "windIndicator", Attr.style (float2ToStyle viewMatrix ( 3, 0 )) ] [])
+            --spacer
+            ::
+                (List.concatMap (viewDebugArrow viewMatrix) model.debugArrows)
         )
 
 
-viewDebugArrow : DebugArrow -> List (Html Msg.Msg)
-viewDebugArrow arrow =
+viewDebugArrow : Matrix3.Float3x3 -> DebugArrow -> List (Html Msg.Msg)
+viewDebugArrow viewMatrix arrow =
     [ div
         [ Attr.class "debugArrowSource"
-        , Attr.style (( "background-color", arrow.color ) :: (float2ToStyle arrow.start))
+        , Attr.style (( "background-color", arrow.color ) :: (float2ToStyle viewMatrix arrow.start))
         ]
         []
     , div
         [ Attr.class "debugArrowEnd"
-        , Attr.style (( "background-color", arrow.color ) :: (float2ToStyle (Vec2.add arrow.start arrow.vector)))        
+        , Attr.style (( "background-color", arrow.color ) :: (float2ToStyle viewMatrix (Vec2.add arrow.start arrow.vector)))
         ]
         []
     , div
         [ Attr.class "debugArrowCaption"
-        , Attr.style (( "color", arrow.color ) :: (float2ToStyle (Vec2.add arrow.start arrow.vector)))        
+        , Attr.style (( "color", arrow.color ) :: (float2ToStyle viewMatrix (Vec2.add arrow.start arrow.vector)))
         ]
-        [ text arrow.name]
+        [ text arrow.name ]
     ]
+
+
+computeViewMatrix : Model -> Matrix3.Float3x3
+computeViewMatrix model =
+    let
+        ( playerX, playerY ) =
+            model.playerPos
+
+        playerPosScaled =
+            ( GameConstants.viewScaleX * playerX, GameConstants.viewScaleY * playerY )
+
+        ( shiftX, shiftY ) =
+            Vec2.sub GameConstants.playerPosOnScreen playerPosScaled
+    in
+        ( ( GameConstants.viewScaleX, 0, shiftX )
+        , ( 0, GameConstants.viewScaleY, shiftY )
+        , ( 0, 0, 1 )
+        )
